942a236d3b8fb52a925564f0ce0e6578e2096173
Vilen Hayrapetyan <vilen.hayrapetyan@gmail.com>
2021-07-18 21:26:00 +0400

    Test16

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -45,7 +45,7 @@ resource "aws_lb_listener" "forward_api" {
 # =================================
 resource "aws_lb_target_group" "api_tg" {
   name     = "api-lb-tg"
-  port     = 8080
+  port     = 80
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.my-vpc.id
 }
