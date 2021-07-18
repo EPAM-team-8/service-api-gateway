7309fecb38836377a5f54d0b626f6b1ede39103b
Vilen Hayrapetyan <vilen.hayrapetyan@gmail.com>
2021-07-18 20:27:56 +0400

    Test1

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -4,8 +4,8 @@ resource "aws_route53_record" "api-lb-record" {
   type    = "A"
 
   alias {
-      name                   = aws_lb.api-alb.dns_name
-      zone_id                = aws_lb.api-alb.zone_id
+      name                   = aws_lb.api_alb.dns_name
+      zone_id                = aws_lb.api_alb.zone_id
       evaluate_target_health = true
   }
 }
