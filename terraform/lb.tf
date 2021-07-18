32630c317de2745c47c6aa33a3ab81461dab066e
Vilen Hayrapetyan <vilen.hayrapetyan@gmail.com>
2021-07-18 20:32:20 +0400

    Test4

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -47,7 +47,7 @@ resource "aws_lb_target_group" "api_tg" {
   name     = var.aws_service_name
   port     = var.http_tcp_listener_port
   protocol = "HTTP"
-  vpc_id   = data.aws_vpc.main.id
+  vpc_id   = data.aws_vpc.my-vpc.id
 }
 
 # =================================
