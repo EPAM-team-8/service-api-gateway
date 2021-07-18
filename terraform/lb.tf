41374bb80bed0f709c5bef951810f2bc7694cc42
Vilen Hayrapetyan <vilen.hayrapetyan@gmail.com>
2021-07-18 21:23:44 +0400

    Change target group port

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -45,7 +45,7 @@ resource "aws_lb_listener" "forward_api" {
 # =================================
 resource "aws_lb_target_group" "api_tg" {
   name     = "api-lb-tg"
-  port     = 80
+  port     = 8080
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.my-vpc.id
 }
